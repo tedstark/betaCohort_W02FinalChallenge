02a56cf9dbcbceb7694f2b01a303a808d22fb59c
Ted <ted.stark@gmail.com>
2018-01-20 10:06:06 -0700

    Attempted use of Arrays

--- src/com/company/BankAccount.java
+++ src/com/company/BankAccount.java
@@ -1,10 +1,16 @@
 package com.company;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BankAccount {
     public int baNumber;
     public String baOwner;
     public double baBalance;
-
+    public double newBalance;
+    //public ArrayList<Integer> accountList = new ArrayList<>();
+    //DecimalFormat moneyFormat = new DecimalFormat("##.00");
 
     public BankAccount(String name, int account, double balance) {
         //constructor
@@ -12,15 +18,22 @@ public class BankAccount {
         this.baNumber = account;
         this.baOwner = name;
         System.out.println("New bank account for " + baOwner + " was created (" + baNumber + ")");
-    }
+        //accountList.add(account);
+            }
 
+    public double deposit(double amount) {
+        newBalance = baBalance + amount;
+        return newBalance;
+    }
     public double withdraw(double amount) {
         baBalance = baBalance - amount;
         return baBalance;
     }
 
-    public double deposit(double amount) {
-        baBalance = baBalance + amount;
-        return baBalance;
+//    public ArrayList<Integer> getAccountList() {
+//        System.out.println(accountList);
+//        return accountList;
+
     }
-}
+
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,9 @@
 package com.company;
 
+import java.sql.Array;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -24,62 +27,80 @@ public class Main {
 //        []Look up the 'for each' loop and try to apply that, rather than a 'fori' loop (we will cover this later in class).
 //        []Look up 'getters and setters' and see if you can apply the concept (we will cover this later in class)
 
+        int ba[] = new int[9];
+        double bb[] = new double[9];
+        DecimalFormat moneyFormat = new DecimalFormat("##.00");
+
         //create accounts
-        BankAccount Account001 = new BankAccount("Jake T", 12345601, 3513.45);
-        BankAccount Account002 = new BankAccount("Koddie B", 12345602, 1436.90);
-        BankAccount Account003 = new BankAccount("Zech H", 12345603, 1367.34);
-        BankAccount Account004 = new BankAccount("Lori T", 12345604, 2438.45);
-        BankAccount Account005 = new BankAccount("Joan A", 12345605, 2100.89);
-        BankAccount Account006 = new BankAccount("Kym H", 12345606, 2876.78);
-        BankAccount Account007 = new BankAccount("Mark F",12345607, 1756.24);
-        BankAccount Account008 = new BankAccount("Manny A",12345608, 3011.15);
-        BankAccount Account009 = new BankAccount("Ted S", 12345609,1250.50);
+        BankAccount Account001 = new BankAccount("Jake T", 12345601, 50000.11);
+        BankAccount Account002 = new BankAccount("Koddie B", 12345602, 50000.12);
+        BankAccount Account003 = new BankAccount("Zech H", 12345603, 50000.13);
+        BankAccount Account004 = new BankAccount("Lori T", 12345604, 50000.14);
+        BankAccount Account005 = new BankAccount("Joan A", 12345605, 50000.15);
+        BankAccount Account006 = new BankAccount("Kym H", 12345606, 50000.16);
+        BankAccount Account007 = new BankAccount("Mark F", 12345607, 50000.17);
+        BankAccount Account008 = new BankAccount("Manny A", 12345608, 50000.18);
+        BankAccount Account009 = new BankAccount("Ted S", 12345609, 50000.19);
+        System.out.println("");
+
+        ba[0]= (Account001.baNumber);
+        ba[1]= (Account002.baNumber);
+
+        bb[0]=(Account001.baBalance);
+        bb[1]=(Account002.baBalance);
+
+        System.out.println(Arrays.toString(ba));
+        System.out.println((Arrays.toString(bb)));
 
         //deposit amount into accounts, version 1
         System.out.println("");
-        System.out.println("Deposit $57000 into account "+Account001.baNumber +".");
-        Account001.deposit (57000);
-        System.out.println("New balance for account " + Account001.baNumber + " is $" + Account001.baBalance +".");
+        System.out.println("Deposit $57000 into account " + Account001.baNumber + ".");
+        Account001.deposit(57000.0);
+        System.out.println("New balance for account " + Account001.baNumber + " is $" + Account001.baBalance + ".");
+        bb[1]=(Account002.newBalance);
 
         System.out.println("");
-        System.out.println("Deposit $57000 into account "+Account002.baNumber +".");
-        Account002.deposit (57000);
-        System.out.println("New balance for account " + Account002.baNumber + " is $" + Account002.baBalance +".");
+        System.out.println("Deposit $57000 into account " + Account002.baNumber + ".");
+        Account002.deposit(57000.0);
+        System.out.println("New balance for account " + Account002.baNumber + " is $" + Account002.baBalance + ".");
+
 
         System.out.println("");
-        System.out.println("Deposit $57000 into account "+Account003.baNumber +".");
-        Account003.deposit (57000);
-        System.out.println("New balance for account " + Account003.baNumber + " is $" + Account003.baBalance +".");
+        System.out.println("Deposit $57000 into account " + Account003.baNumber + ".");
+        Account003.deposit(57000);
+        System.out.println("New balance for account " + Account003.baNumber + " is $" + Account003.baBalance + ".");
 
         System.out.println("");
-        System.out.println("Deposit $57000 into account "+Account004.baNumber +".");
-        Account004.deposit (57000);
-        System.out.println("New balance for account " + Account004.baNumber + " is $" + Account004.baBalance +".");
+        System.out.println("Deposit $57000 into account " + Account004.baNumber + ".");
+        Account004.deposit(57000);
+        System.out.println("New balance for account " + Account004.baNumber + " is $" + Account004.baBalance + ".");
 
         System.out.println("");
-        System.out.println("Deposit $57000 into account "+Account005.baNumber +".");
-        Account005.deposit (57000);
-        System.out.println("New balance for account " + Account005.baNumber + " is $" + Account005.baBalance +".");
+        System.out.println("Deposit $57000 into account " + Account005.baNumber + ".");
+        Account005.deposit(57000);
+        System.out.println("New balance for account " + Account005.baNumber + " is $" + Account005.baBalance + ".");
 
         System.out.println("");
-        System.out.println("Deposit $57000 into account "+Account006.baNumber +".");
-        Account006.deposit (57000);
-        System.out.println("New balance for account " + Account006.baNumber + " is $" + Account006.baBalance +".");
+        System.out.println("Deposit $57000 into account " + Account006.baNumber + ".");
+        Account006.deposit(57000);
+        System.out.println("New balance for account " + Account006.baNumber + " is $" + Account006.baBalance + ".");
 
         System.out.println("");
-        System.out.println("Deposit $57000 into account "+Account007.baNumber +".");
-        Account007.deposit (57000);
-        System.out.println("New balance for account " + Account007.baNumber + " is $" + Account007.baBalance +".");
+        System.out.println("Deposit $57000 into account " + Account007.baNumber + ".");
+        Account007.deposit(57000);
+        System.out.println("New balance for account " + Account007.baNumber + " is $" + Account007.baBalance + ".");
 
         System.out.println("");
-        System.out.println("Deposit $57000 into account "+Account008.baNumber +".");
-        Account008.deposit (57000);
-        System.out.println("New balance for account " + Account008.baNumber + " is $" + Account008.baBalance +".");
+        System.out.println("Deposit $57000 into account " + Account008.baNumber + ".");
+        Account008.deposit(57000);
+        System.out.println("New balance for account " + Account008.baNumber + " is $" + Account008.baBalance + ".");
 
         System.out.println("");
-        System.out.println("Deposit $57000 into account "+Account009.baNumber +".");
-        Account009.deposit (57000);
-        System.out.println("New balance for account " + Account009.baNumber + " is $" + Account009.baBalance +".");
+        System.out.println("Deposit $57000 into account " + Account009.baNumber + ".");
+        Account009.deposit(57000);
+        System.out.println("New balance for account " + Account009.baNumber + " is $" + Account009.baBalance + ".");
+
+        System.out.println((Arrays.toString(bb)));
 
         System.out.println("");
         System.out.println("Have a nice day!");
